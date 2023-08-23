e0a042f6cac3ca62deb882f12307d5aed5e3fb7f
mmisha2504 <108689311+mmisha2504@users.noreply.github.com>
2023-08-23 14:48:17 +0300

    Update Music.jsx

--- src/Components/Navbar/Music.jsx
+++ src/Components/Navbar/Music.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import s from "./Music.module.css";
 
 const Music = (props)=>{
-    return <div> Music</div>
+    console.log (Music)  
+        return <div> Music</div>
 }
 
 export default Music;
